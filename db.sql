85a835721db44f634c6b914c31b1d9ae17ce13bb
fjchen7 <fjchen7@gmail.com>
2024-07-05 19:00:21 +0800

    Modify DB

--- db.sql
+++ db.sql
@@ -1,22 +1,9 @@
 CREATE TABLE action_url (
-    path CHAR(6) PRIMARY KEY,  -- url path
+    id CHAR(6) PRIMARY KEY,  -- url
     creator: VARCHAR(50) NOT NULL,
-    commission: DECIMAL(10, 2) NOT NULL,
-    metadata: JSON,  -- title, description, image, content ...
+    metadata: JSON,  -- title, description, image, content, commission ...
     component: JSON,
     action_id: VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE action_url_preview (
-    path CHAR(6) PRIMARY KEY,  -- url path
-    creator: VARCHAR(50) NOT NULL,
-    commission: DECIMAL(10, 2) NOT NULL,
-    metadata: JSON,  -- title, description, image, content ...
-    component: JSON,
-    action_id: VARCHAR(50) NOT NULL,
-    expired_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
