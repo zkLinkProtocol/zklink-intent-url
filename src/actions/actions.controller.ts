b84d3eec302910574103cf34706c572f7cd848d3
fjchen7 <fjchen7@gmail.com>
2024-07-08 13:53:41 +0800

    Extend BaseController in ActionsController

--- src/actions/actions.controller.ts
+++ src/actions/actions.controller.ts
@@ -11,13 +11,19 @@ import { ActionsService } from './actions.service';
 import { ActionId } from './adapter';
 import { Action, ActionMetadata } from './interface';
 import { Transaction } from 'ethers';
+import { BaseController } from 'src/common/base.controller';
+import { ResponseDto } from 'src/common/response.dto';
+import { CommonApiOperation } from 'src/common/base.decorators';
 
 @Controller('actions')
-export class ActionsController {
-  constructor(private readonly actionsService: ActionsService) {}
+export class ActionsController extends BaseController {
+  constructor(private readonly actionsService: ActionsService) {
+    super();
+  }
 
   @Get()
-  findAll(): { [id: ActionId]: ActionMetadata } {
+  @CommonApiOperation('Get all actions.')
+  findAll(): Promise<ResponseDto<{ [id: ActionId]: ActionMetadata }>> {
     const actions = this.actionsService.findAll();
     const actionsMetadata: { [id: ActionId]: ActionMetadata } = Object.keys(
       actions,
@@ -28,24 +34,30 @@ export class ActionsController {
       },
       {} as { [id: ActionId]: ActionMetadata },
     );
-    return actionsMetadata;
+    return this.success(actionsMetadata);
   }
 
   @Get(':id')
-  find(@Param('id') id: ActionId): Action {
-    return this.actionsService.find(id);
+  @CommonApiOperation('Get action by Id.')
+  find(@Param('id') id: ActionId): Promise<ResponseDto<Action>> {
+    return this.success(this.actionsService.find(id));
   }
 
   @Post(':id/transaction/generate')
+  @CommonApiOperation('Generate transaction by action Id.')
   generateTransaction(
     @Param('id') id: ActionId,
     @Body() body: any[],
-  ): Transaction {
-    return this.actionsService.generateTransaction(id, body);
+  ): Promise<ResponseDto<Transaction>> {
+    return this.success(this.actionsService.generateTransaction(id, body));
   }
 
   @Post(':id/transaction/post')
-  postTransaction(@Param('id') id: ActionId, @Body() tx: Transaction): boolean {
-    return this.actionsService.postTransaction(id, tx);
+  @CommonApiOperation('Execute post transaction function by action Id.')
+  postTransaction(
+    @Param('id') id: ActionId,
+    @Body() tx: Transaction,
+  ): Promise<ResponseDto<boolean>> {
+    return this.success(this.actionsService.postTransaction(id, tx));
   }
 }
