7efd0c0aed9bd67b5f28b586d9014145d834eefd
fjchen7 <fjchen7@gmail.com>
2024-07-08 22:03:23 +0800

    Implement actions adapter

--- src/actions/adapter.ts
+++ src/actions/adapter.ts
@@ -1,3 +1,6 @@
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import { Logger } from '@nestjs/common';
 import { Action } from './interface';
 
 export type ActionId = string;
@@ -21,4 +24,33 @@ export class RegisteredActions {
   }
 }
 
-export const registeredActions = new RegisteredActions();
+const logger = new Logger('adapter');
+
+export function loadActions(): RegisteredActions {
+  function isAction(obj: any): obj is Action {
+    return (
+      typeof obj.getMetadata === 'function' &&
+      typeof obj.generateTransaction === 'function' &&
+      typeof obj.postTransaction === 'function'
+    );
+  }
+
+  const actions = new RegisteredActions();
+  const actionImplPath = join(__dirname, '..', 'action-impl');
+  const dirs = readdirSync(actionImplPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+  for (const dir of dirs) {
+    const module = require(join(actionImplPath, dir, 'index'));
+    if (module.default && isAction(module.default)) {
+      actions.register(dir, module.default);
+      logger.log(`Loaded action "${dir}"`);
+    }
+  }
+
+  return actions;
+}
+
+const registeredActions = loadActions();
+export { registeredActions };
