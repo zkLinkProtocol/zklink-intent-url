38f3fee3d9c4069b50de1fc138b2304d3c3f5191
xsteadybcgo <xw4585@gmail.com>
2024-07-15 11:14:37 +0800

    fix: remove stale files

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE action_url (
-    id CHAR(6) PRIMARY KEY,  -- url
-    creator: VARCHAR(50) NOT NULL,
-    metadata: JSON,  -- title, description, image, content, commission ...
-    component: JSON,
-    action_id: VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
