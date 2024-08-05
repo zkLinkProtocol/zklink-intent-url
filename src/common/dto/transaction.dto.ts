19ade0daa16254ba370cdd0a8ef3ef1d76fa4339
robert <robert@zklink.org>
2024-08-05 15:25:19 +0800

    Structured return values.

--- src/common/dto/transaction.dto.ts
+++ src/common/dto/transaction.dto.ts
@@ -2,11 +2,53 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsString } from 'class-validator';
 import { ContractTransaction, JsonRpcProvider } from 'ethers';
 
+export class Tx {
+  @ApiProperty({ description: 'The chain id of transaction' })
+  chainId: number;
+
+  @ApiProperty({ description: 'The to address of transaction' })
+  to: string;
+
+  @ApiProperty({
+    description: 'The value of the transaction (including decimal)',
+  })
+  value: string;
+
+  @ApiProperty({
+    description: 'The data of the transaction',
+  })
+  data: string;
+
+  @ApiProperty({
+    description: 'Transaction parameters',
+  })
+  dataObject: object;
+}
+
+export class Token {
+  @ApiProperty({ description: 'The chain id of the target chain' })
+  chainId: number;
+
+  @ApiProperty({ description: 'The token address of the target chain' })
+  token: string;
+
+  @ApiProperty({
+    description:
+      'The number of tokens required by the target chain (including decimal)',
+  })
+  amount: string;
+}
+
 export class GeneratedTransaction {
   @ApiProperty({ description: 'The transaction request object' })
-  tx: ContractTransaction;
+  txs: Tx[];
 
-  @ApiProperty({ description: 'The transaction request object' })
+  @ApiProperty({
+    description: 'Token information required by the target chain',
+  })
+  tokens: Token[];
+
+  @ApiProperty({ description: 'The provider of transaction' })
   provider: JsonRpcProvider;
 
   @ApiProperty({
@@ -14,10 +56,4 @@ export class GeneratedTransaction {
   })
   @IsBoolean()
   shouldSend: boolean;
-
-  @ApiPropertyOptional({
-    description: 'Cross-chain transfer amount',
-  })
-  @IsString()
-  crossChainAmount?: string;
 }
