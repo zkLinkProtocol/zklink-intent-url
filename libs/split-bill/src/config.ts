a61cbddb295a6824bbfbf8ae58a1c9ccfd8a741a
Hedy-chu <hechu1213@gmail.com>
2024-09-05 15:23:00 +0800

    update config

--- libs/split-bill/src/config.ts
+++ libs/split-bill/src/config.ts
@@ -14,6 +14,16 @@ export const metadata = {
       chainId: '810180',
       contractAddress: '0x',
     },
+    {
+      name: 'zkLink Nova sepolia',
+      chainId: '810181',
+      contractAddress: '0x',
+    },
+    {
+      name: 'zkLink dev',
+      chainId: '270',
+      contractAddress: '0x',
+    },
   ],
   dApp: { name: 'Split bill' },
   author: { name: 'zkLink', github: 'https://github.com/zkLinkProtocol' },
