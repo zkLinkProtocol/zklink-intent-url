c3ad500ec2c20204920cc02e5db07f992560d740
Hedy-chu <hechu1213@gmail.com>
2024-09-06 21:46:18 +0800

    add buy-me-coffee module

--- libs/registry/src/registry.module.ts
+++ libs/registry/src/registry.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { BuyMeACoffeeService } from '@action/buy-me-a-coffee';
+import { BuyMeACoffeeModule } from '@action/buy-me-a-coffee';
 import { CrossChainSwapService } from '@action/cross-chain-swap';
 import { NovaswapService } from '@action/novaswap';
 import { RedEnvelopeService } from '@action/red-envelope';
@@ -9,10 +9,10 @@ import { SplitBillService } from 'libs/split-bill/src';
 import { RegistryService } from './registry.service';
 
 @Module({
+  imports: [BuyMeACoffeeModule],
   providers: [
     RegistryService,
     CrossChainSwapService,
-    BuyMeACoffeeService,
     NovaswapService,
     RedEnvelopeService,
     SplitBillService,
@@ -20,7 +20,6 @@ import { RegistryService } from './registry.service';
   exports: [
     RegistryService,
     CrossChainSwapService,
-    BuyMeACoffeeService,
     NovaswapService,
     RedEnvelopeService,
     SplitBillService,
