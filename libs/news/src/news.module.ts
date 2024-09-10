9ee7ec35b12c0d9b78939492e006913a696babe8
robin <robin@zklink.org>
2024-09-10 16:06:00 +0800

    fix news  module

--- libs/news/src/news.module.ts
+++ libs/news/src/news.module.ts
@@ -1,10 +1,11 @@
+import { CrossChainSwapModule } from '@action/cross-chain-swap';
 import { Module } from '@nestjs/common';
 import { ActionUrlModule } from 'src/modules/actionUrl/actionUrl.module';
 
 import { NewsService } from './news.service';
 
 @Module({
-  imports: [ActionUrlModule],
+  imports: [ActionUrlModule, CrossChainSwapModule],
   providers: [NewsService],
   exports: [NewsService, ActionUrlModule],
 })
