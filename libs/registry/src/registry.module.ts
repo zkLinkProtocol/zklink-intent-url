34a6b6b4f53b1d481b39678101a3cf9473a12f1e
cam <hao286242341@gmail.com>
2024-09-10 15:11:48 +0800

    update registry

--- libs/registry/src/registry.module.ts
+++ libs/registry/src/registry.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { BuyMeACoffeeModule } from '@action/buy-me-a-coffee';
 import { CrossChainSwapService } from '@action/cross-chain-swap';
 import { NovaswapService } from '@action/novaswap';
+import { PumpFunModule } from '@action/pump-fun';
 import { RedEnvelopeService } from '@action/red-envelope';
 import { SplitBillModule } from '@action/slipt-bill';
 
 import { RegistryService } from './registry.service';
 
 @Module({
-  imports: [BuyMeACoffeeModule, SplitBillModule],
+  imports: [BuyMeACoffeeModule, SplitBillModule, PumpFunModule],
   providers: [
     RegistryService,
     CrossChainSwapService,
