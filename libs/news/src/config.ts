47c95192d02bce1246d69225de0ab9afc63c1172
robin <robin@zklink.org>
2024-09-10 15:36:49 +0800

    add news action

---
+++ libs/news/src/config.ts
@@ -0,0 +1,65 @@
+import { ActionMetadata } from 'src/common/dto';
+
+export type FormName = 'amountToBuy' | 'tokenIn' | 'tokenOutAddress';
+
+export const METADATA: ActionMetadata<FormName> = {
+  title: 'news',
+  description: 'Perform news seamlessly across multiple networks',
+  logo: 'https://placehold.co/40x40',
+  networks: [
+    {
+      name: 'Arbitrum',
+      chainId: '42161',
+      contractAddress: '0x',
+    },
+  ],
+  dApp: { name: 'News' },
+  author: { name: 'zkLink', github: 'https://github.com/zkLinkProtocol' },
+  magicLinkMetadata: {},
+  intent: {
+    components: [
+      {
+        name: 'amountToBuy',
+        label: 'Amount to Buy',
+        desc: 'The amount of input tokens used to buy output tokens',
+        type: 'input',
+        regex: '^[0-9]+(.[0-9]+)?$',
+        regexDesc: 'Positive number',
+      },
+      {
+        name: 'tokenIn',
+        label: 'Token In ',
+        desc: 'The token you want to swap',
+        type: 'searchSelect',
+        regex: '^[a-zA-Z0-9]+$',
+        regexDesc: 'Token Symbol',
+        options: [
+          {
+            label: 'WETH',
+            value: 'weth',
+          },
+          {
+            label: 'WBTC',
+            value: 'wbtc',
+          },
+          {
+            label: 'USDT',
+            value: 'usdt',
+          },
+          {
+            label: 'USDC',
+            value: 'usdc',
+          },
+        ],
+      },
+      {
+        name: 'tokenOutAddress',
+        label: 'Token Out Address',
+        desc: 'The address of the token you want to receive',
+        type: 'input',
+        regex: '^0x[a-fA-F0-9]{40}$',
+        regexDesc: 'Address',
+      },
+    ],
+  },
+};
--- libs/registry/src/registry.module.ts
+++ libs/registry/src/registry.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { BuyMeACoffeeService } from '@action/buy-me-a-coffee';
 import { CrossChainSwapService } from '@action/cross-chain-swap';
+import { NewsService } from '@action/news';
 import { NovaswapService } from '@action/novaswap';
 import { RedEnvelopeService } from '@action/red-envelope';
 import { SplitBillService } from 'libs/split-bill/src';
@@ -16,6 +17,7 @@ import { RegistryService } from './registry.service';
     NovaswapService,
     RedEnvelopeService,
     SplitBillService,
+    NewsService,
   ],
   exports: [
     RegistryService,
@@ -24,6 +26,7 @@ import { RegistryService } from './registry.service';
     NovaswapService,
     RedEnvelopeService,
     SplitBillService,
+    NewsService,
   ],
 })
 export class RegistryModule {}
