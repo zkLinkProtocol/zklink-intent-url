ee510d1d8a72bbe6ad1f41cd707d077037c6a1d4
Hedy-chu <hechu1213@gmail.com>
2024-09-11 17:20:52 +0800

    update text

--- libs/red-envelope/src/metadata.ts
+++ libs/red-envelope/src/metadata.ts
@@ -16,6 +16,7 @@ export const genMetadata = (configValue: Value): ActionMetadata<FormName> => ({
   dApp: { name: 'Red Packet 🧧' },
   author: { name: 'zkLink', github: 'https://github.com/zkLinkProtocol' },
   magicLinkMetadata: {
+    title: 'Red Packet 🧧',
     description: 'Best wishes!',
   },
   intent: {
@@ -41,7 +42,7 @@ export const genMetadata = (configValue: Value): ActionMetadata<FormName> => ({
       {
         name: 'totalDistributionAmount',
         label: 'Total Token Amount',
-        desc: 'The total number of tokens to be distributed',
+        desc: 'The total amount of tokens to be distributed',
         type: 'input',
         regex: '^[1-9]\\d*$',
         regexDesc: 'Int',
@@ -65,18 +66,18 @@ export const genMetadata = (configValue: Value): ActionMetadata<FormName> => ({
       },
       {
         name: 'gasToken',
-        label: 'Gas Token',
+        label: 'Who should pay for the claiming gas fee',
         desc: 'Gas can be deducted from distributed amount, allowing recipient to grab red envelope with 0 gas',
         type: 'searchSelect',
         regex: '^[a-zA-Z0-9]+$',
         regexDesc: 'String',
         options: [
           {
-            label: 'ETH(Pay By Recipient)',
+            label: 'Recipient',
             value: GasTokenValue.Eth,
           },
           {
-            label: 'Distributed Token(No Gas)',
+            label: 'Red Packet Creator (Requires additional tokens)',
             value: GasTokenValue.DistributedToken,
           },
         ],
