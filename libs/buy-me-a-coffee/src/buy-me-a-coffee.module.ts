1cff75b51520c4cc05f5ff6402e788fd919bff8a
jarmo <xw4585@gmail.com>
2024-09-27 14:50:16 +0800

    fix: data module import path (#229)

--- libs/buy-me-a-coffee/src/buy-me-a-coffee.module.ts
+++ libs/buy-me-a-coffee/src/buy-me-a-coffee.module.ts
@@ -1,11 +1,11 @@
+import { DataModule } from '@core/data';
 import { Module } from '@nestjs/common';
 
 import { BuyMeACoffeeService } from './buy-me-a-coffee.service';
-import { DataModule } from '../../data/src/data.module';
 
 @Module({
   imports: [DataModule],
   providers: [BuyMeACoffeeService],
-  exports: [BuyMeACoffeeService, DataModule],
+  exports: [BuyMeACoffeeService],
 })
 export class BuyMeACoffeeModule {}
--- libs/split-bill/src/split-bill.module.ts
+++ libs/split-bill/src/split-bill.module.ts
@@ -1,11 +1,11 @@
+import { DataModule } from '@core/data';
 import { Module } from '@nestjs/common';
 
 import { SplitBillService } from './split-bill.service';
-import { DataModule } from '../../data/src/data.module';
 
 @Module({
   imports: [DataModule],
   providers: [SplitBillService],
-  exports: [SplitBillService, DataModule],
+  exports: [SplitBillService],
 })
 export class SplitBillModule {}
