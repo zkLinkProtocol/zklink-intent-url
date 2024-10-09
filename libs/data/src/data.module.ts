f3dddd9f473bdaa8167c97f0ea1e0797e643d87c
xsteadybcgo <xw4585@gmail.com>
2024-10-09 18:12:23 +0800

    fix: CreatorRepository

--- libs/data/src/data.module.ts
+++ libs/data/src/data.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 
-import { IntentionRepository } from 'src/repositories';
-import { IntentionRecordRepository } from 'src/repositories';
+import {
+  CreatorRepository,
+  IntentionRecordRepository,
+  IntentionRepository,
+} from 'src/repositories';
 import { UnitOfWorkModule } from 'src/unitOfWork';
 
 import { DataService } from './data.service';
 
 @Module({
   imports: [UnitOfWorkModule],
-  providers: [DataService, IntentionRepository, IntentionRecordRepository],
+  providers: [
+    DataService,
+    CreatorRepository,
+    IntentionRepository,
+    IntentionRecordRepository,
+  ],
   exports: [DataService],
 })
 export class DataModule {}
