9706e34f7c4ffdc79b000c68e8854d535cd693b6
jarmo <xw4585@gmail.com>
2024-10-09 20:47:16 +0800

    feat: send meme packet msg (#252)
    * feat: send meme redpacket message to bot
    
    * fix: CreatorRepository

--- libs/shared-red-packet/src/config.ts
+++ libs/shared-red-packet/src/config.ts
@@ -4,12 +4,14 @@ export const configuration = {
     networkName: 'zkLink Nova Sepolia',
     rpcUrl: 'https://sepolia.rpc.zklink.io',
     redPacketContractAddress: '0x70E17f5A10f88e8F40F602338a979635699EE5e5',
+    magicLinkUrl: 'https://magic-test.zklink.io/intent',
   },
   prod: {
     chainId: 810180,
     networkName: 'zkLink Nova',
     rpcUrl: 'https://rpc.zklink.io',
     redPacketContractAddress: '0x583ba818E70418c99829F21Dce4A0A188bbc871F',
+    magicLinkUrl: 'https://magic.zklink.io/intent',
   },
 };
 
--- libs/shared-red-packet/src/shared-red-packet.module.ts
+++ libs/shared-red-packet/src/shared-red-packet.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { DataModule } from '@core/data';
+import { TgbotModule } from 'src/modules/tgbot/tgbot.module';
 
 import { SharedRedPacketService } from './shared-red-packet.service';
 
 @Module({
-  imports: [DataModule],
+  imports: [DataModule, TgbotModule],
   providers: [SharedRedPacketService],
   exports: [SharedRedPacketService],
 })
