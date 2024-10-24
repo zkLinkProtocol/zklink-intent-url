2496645e333a3e6f3c76046edf0c9fb811edf7c0
xsteadybcgo <xw4585@gmail.com>
2024-10-24 18:19:34 +0800

    fix: typing

--- src/common/dto/action.dto.ts
+++ src/common/dto/action.dto.ts
@@ -39,12 +39,10 @@ export type GenerateTransactionParams<
 
 export type UpdateFieldType<
   T extends Record<string, any>, // This is the type used in GenerateFormParams
-  K extends keyof T | undefined, // The field to modify (optional)
-> = K extends keyof T
-  ? Omit<ValidateFormData<T>, K> & {
-      [key in K]: T[K][];
-    } // If field is provided, set it to an array of its original type
-  : ValidateFormData<T>;
+  K extends keyof T, // The field to modify (optional)
+> = Omit<ValidateFormData<T>, K> & {
+  [key in K]: T[K][];
+};
 
 export abstract class Action<
   T extends Record<string, any> = Record<string, any>,
