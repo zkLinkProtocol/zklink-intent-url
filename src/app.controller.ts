f8a4cdf46a3baae7c314d9800a722424ffbb8643
robin <robin@zklink.org>
2024-10-31 17:05:01 +0800

    feat: add menu api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
-import { AppService } from './app.service';
+import { CommonApiOperation } from './common/base.decorators';
+import { MENUS, MagicNewsMenu } from './config/menu';
+import { ActionService } from './modules/action/action.service';
+import { GetCreator, OptionLogin } from './modules/auth/creator.decorators';
+import { JwtAuthGuard } from './modules/auth/jwtAuth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly actionService: ActionService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('menu')
+  @CommonApiOperation('Get the dashboard left menu')
+  @OptionLogin()
+  @UseGuards(JwtAuthGuard)
+  async getHello(@GetCreator() creator: { address?: string }) {
+    if (!creator?.address) {
+      return MENUS;
+    }
+
+    const whiteListPermission = await this.actionService.checkActionWhitelist(
+      'news',
+      creator.address,
+    );
+
+    if (!whiteListPermission) {
+      return MENUS;
+    }
+
+    return [...MENUS, MagicNewsMenu];
   }
 }
---
+++ src/config/menu.ts
@@ -0,0 +1,41 @@
+export const MENUS = [
+  {
+    name: 'Portfolio',
+    path: 'portfolio',
+    desc: 'Manage Your Passkey',
+    img: 'layers.svg',
+  },
+  {
+    name: 'Magic Marketplace',
+    path: 'buy',
+    desc: 'Try it now',
+    img: 'bag.svg',
+    isComingSoon: true,
+  },
+  {
+    name: 'Create magicLink',
+    path: 'intent',
+    desc: 'Dummy Mode',
+    img: 'PlusCircle.svg',
+  },
+  {
+    name: 'My magicLink',
+    path: 'my-intent-url',
+    desc: 'Developer Mode',
+    img: 'link.svg',
+  },
+  {
+    name: 'Account Management',
+    path: 'account-management',
+    desc: 'Developer Mode',
+    img: 'Star.svg',
+    isComingSoon: true,
+  },
+];
+
+export const MagicNewsMenu = {
+  name: 'Magic News',
+  path: 'magic-news',
+  desc: 'Developer Mode',
+  img: 'icon-switch.svg',
+};
